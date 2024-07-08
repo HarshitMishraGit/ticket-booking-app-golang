fddb82ca4836f729b92bac278f90c68e579af957
Harshit Mishra <harshitlove28@outlook.com>
2024-07-09 00:20:47 +0530

    Variable with type inference

--- main.go
+++ main.go
@@ -1,6 +1,14 @@
 package main
 
+import "fmt"
+
 func main() {
 	// Call the function
 	print("Hello, World!")
+
+	var x = 5
+	print("variable x = ", x)
+
+	var str string = "Hello, World!"
+	fmt.Printf("str = %s\n", str)
 }
