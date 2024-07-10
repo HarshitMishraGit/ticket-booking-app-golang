e42f9c17ac686378b76dfa4f55f8eb51f021ccbc
Harshit Mishra <harshitlove28@outlook.com>
2024-07-10 16:09:25 +0530

    for loop and how to use it

--- main.go
+++ main.go
@@ -3,25 +3,23 @@ package main
 import "fmt"
 
 func main() {
-	// Call the function
-	print("Hello, World!")
-
-	var x = 5
-	print("variable x = ", x)
-
-	var str string = "Hello, World!"
-	fmt.Printf("str = %s\n", str)
-
-	// Read input from the user
-	var firstName string
-	fmt.Print("Enter your first name: ")
-	fmt.Scan(&firstName)
-
-	fmt.Printf("Hello, %s\n", firstName)
 
 	var booking []string
-	booking = append(booking, "Hotel")
-	fmt.Print("Booking: ", booking, "\n")
+	var remaingSeats uint = 5
+	for {
+		// Read input from the user
+		var firstName string
+		fmt.Print("Enter your first name: ")
+		fmt.Scan(&firstName)
+		_ = append(booking, firstName) // _ is used to decalre a variable that is not used
+		fmt.Printf("Hello, %s\n", firstName)
+		remaingSeats = remaingSeats - 1
+		fmt.Println("Remaining seats : ", remaingSeats)
 
-	 
+		if remaingSeats == 0 {
+			fmt.Println("All seats are booked")
+			break
+		}
+	}
+
 }
